23d6efcdb6bc942ce30153ef04ef07f02c9c30ad
AkilASC2024 <akilkamara5@gmail.com>
2024-07-31 17:38:53 -0400

    finished

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let nounInput = document.getElementById("noun");
 let verbInput = document.getElementById("verb");
 let adjcInput = document.getElementById("adjective");
 let submitButton = document.getElementById("submit_button");
+let form = document.querySelector("form")
+let storyResult = document.getElementById("story_result")
 
 function getTitle(event){
     event.preventDefault();
@@ -13,7 +15,9 @@ function getTitle(event){
 function detectBlanks(event){
     event.preventDefault();
     console.log(titleInput.value)
-    if (titleInput.value) {
+    if (titleInput.value && nounInput.value && verbInput.value && adjcInput.value) {
+        form.style.display = "none"
+        storyResult.innerText = "Last night I ate a " + nounInput.value + ", and today I had to " + verbInput.value + ". What a " + adjcInput.value + " day!"
     }
     else {
         alert("do everything")
